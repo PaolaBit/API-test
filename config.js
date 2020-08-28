6ed57c0a4f5408f56b3041ce1c622274adcc03ab
PaolaBit <paola.cuadros@bit.institute>
2020-08-28 10:30:26 -0500

    Se agregaron ajustes de entorno.

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoDB: process.env.mongoDB || 'mongodb://localhost:27017/falconTravelDB'
+}
---
+++ connectiondb.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+const config = require('./config');
+const connectToDatabase = function() {
+
+    mongoose.connect(config.mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
+        if (err) {
+            console.log("Falló la conexión")
+
+        } else {
+            console.log("Conexión exitosa")
+        }
+
+    })
+}
+
+module.exports = { connectToDatabase }
