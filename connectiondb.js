c9ca56c3dd148f293ca7735e7931217f76fddce6
PaolaBit <paola.cuadros@bit.institute>
2020-08-28 10:53:18 -0500

    Se agregó un comentario

--- connectiondb.js
+++ connectiondb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const config = require('./config');
 const connectToDatabase = function() {
-
+    console.log("config.mongoDB --> ", config.mongoDB)
     mongoose.connect(config.mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
         if (err) {
             console.log("Falló la conexión")
