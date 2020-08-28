ebb2df2c09f209c28a627d10ea2d8e2c97c7aede
PaolaBit <paola.cuadros@bit.institute>
2020-08-28 10:53:31 -0500

    Se agregó un comentario del error

--- connectiondb.js
+++ connectiondb.js
@@ -4,7 +4,7 @@ const connectToDatabase = function() {
     console.log("config.mongoDB --> ", config.mongoDB)
     mongoose.connect(config.mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
         if (err) {
-            console.log("Falló la conexión")
+            console.log("Falló la conexión", err)
 
         } else {
             console.log("Conexión exitosa")
